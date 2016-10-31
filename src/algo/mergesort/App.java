287bd82f8c83f51f3833c89ccf66a37510e11613
Casey Morris <casemorris@hotmail.com>
2016-10-31 09:51:55 -0700

    Fixed merge sort

--- src/algo/mergesort/App.java
+++ src/algo/mergesort/App.java
@@ -5,16 +5,24 @@ public class App
 
 	public static void main(String[] args)
 	{
-		int[] inputArray = { 9,7,3,1,6,3,2,6,8,9,2,3,0};
+		//int[] inputArray = { 9,7,3,1,6,3,2,6,8,9,2,3,0};
+		int[] inputArray = { 9,7,3,1};
 		MergeSort sorter = new MergeSort();
-		
+		display(inputArray);
 		sorter.sort(inputArray);
+		display(inputArray);
 		
+		
+
+	}
+	
+	public static void display(int[] inputArray)
+	{
 		for(int i = 0; i < inputArray.length; i++)
 		{
-			System.out.println(inputArray[i]);
+			System.out.print(inputArray[i] + " ");
 		}
-
+		System.out.println();
 	}
 
 }
--- src/algo/mergesort/MergeSort.java
+++ src/algo/mergesort/MergeSort.java
@@ -3,9 +3,56 @@ package algo.mergesort;
 public class MergeSort
 {
 	
-	private static int[] merge(int[] arrayToSort, int firstIndex, int middleIndex, int lastIndex)
+	private static void merge(int[] arrayToSort, int firstIndex, int middleIndex, int lastIndex)
 	{
+		int tempArray[] = new int[lastIndex - firstIndex + 1];
 		
+		int leftIndex = firstIndex; // Index counter for left side of array
+		int rightIndex = middleIndex + 1; // Index counter for right side of array
+		int counter = 0;
+		
+		while(leftIndex <= middleIndex && rightIndex <= lastIndex)
+		{
+
+		
+			if(arrayToSort[leftIndex] < arrayToSort[rightIndex])
+			{
+				tempArray[counter] = arrayToSort[leftIndex];
+				leftIndex++;
+			}
+			else
+			{
+				tempArray[counter] = arrayToSort[rightIndex];
+				rightIndex++;
+			}
+			counter++;
+		}
+		
+		// Left and right sub-array is sorted
+		if(leftIndex <= middleIndex)
+		{
+			while(leftIndex <= middleIndex)
+			{
+				tempArray[counter] = arrayToSort[leftIndex];
+				leftIndex++;
+				counter++;
+			}
+		}
+		else if(rightIndex <= lastIndex)
+		{
+			while(rightIndex <= lastIndex)
+			{	
+				tempArray[counter] = arrayToSort[rightIndex];
+				rightIndex++;
+				counter++;
+			}
+		}
+
+		// Load back into arrayToSort
+		for(int i  = 0; i< tempArray.length; i++)
+		{
+			arrayToSort[firstIndex + i] = tempArray[i];
+		}
 	}
 	
 	public static void sort(int[] unsortedArray)
@@ -17,10 +64,10 @@ public class MergeSort
 	{
 		if(firstIndex < lastIndex)
 		{
-		int middleIndex = (firstIndex + lastIndex)/2; // Middle of array
-		sort(unsortedArray, firstIndex, middleIndex); // Split left side of array recursively
-		sort(unsortedArray, middleIndex+1, lastIndex); // Split right side of array recursively
-		merge(unsortedArray, firstIndex, middleIndex, lastIndex); // Sort and merge arrays
+			int middleIndex = (firstIndex + lastIndex)/2; // Middle of array
+			sort(unsortedArray, firstIndex, middleIndex); // Split left side of array recursively
+			sort(unsortedArray, middleIndex+1, lastIndex); // Split right side of array recursively
+			merge(unsortedArray, firstIndex, middleIndex, lastIndex); // Sort and merge arrays
 		}
 	}
 	
